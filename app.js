25744f00cd065f9c3408a70af391d8f6bea114a3
darkrazor99 <tarektheman99@gmail.com>
2023-09-30 01:13:06 +0200

    edited toggle nav button

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
 // pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
 // slice extracts a section of a string without modifying original string
 //offsetTop - A Number, representing the top position of the element, in pixels
@@ -13,7 +12,16 @@ const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", () => {
-    linksContainer.classList.toggle("show-links");
+    // linksContainer.classList.toggle("show-links");
+    // doing it dynamicly
+    const containerHeight = linksContainer.getBoundingClientRect().height
+    const linksHeight = links.getBoundingClientRect().height
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = 0;
+    }
+
 
 });
 // ********** fixed navbar ************
