482561c6c0cfffe1f4638bd1c68b897fff6da9c1
darkrazor99 <tarektheman99@gmail.com>
2023-09-30 01:25:08 +0200

    made the nav bar fixed

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
 // slice extracts a section of a string without modifying original string
 //offsetTop - A Number, representing the top position of the element, in pixels
 
@@ -16,15 +15,30 @@ navToggle.addEventListener("click", () => {
     // doing it dynamicly
     const containerHeight = linksContainer.getBoundingClientRect().height
     const linksHeight = links.getBoundingClientRect().height
-    if (containerHeight === 0) {
+    if (containerHeight === 0)
         linksContainer.style.height = `${linksHeight}px`;
-    } else {
+    else
         linksContainer.style.height = 0;
-    }
+
 
 
 });
-// ********** fixed navbar ************
 
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+// ********** fixed navbar ************
+window.addEventListener("scroll", () => {
+    const scrollHeight = window.scrollY;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight)
+        navbar.classList.add("fixed-nav");
+    else
+        navbar.classList.remove("fixed-nav");
+
+    if (scrollHeight > 500)
+        topLink.classList.add("show-link");
+    else
+        topLink.classList.remove("show-link");
+});
 // ********** smooth scroll ************
 // select links
